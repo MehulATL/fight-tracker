93afe20d270b63f9fd2fd5b57ecbd44f65d7fbf2
Mehul Patel <mehul8@gmail.com>
2013-12-16 11:46:20 -0800

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fightTrackerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
